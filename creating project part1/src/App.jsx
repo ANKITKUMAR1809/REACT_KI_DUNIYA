af65ea37d144253a843d984c65e13126749138bb
ANKITKUMAR1809 <skwedsak143@gmail.com>
2025-01-23 16:01:26 +0530

    part 2 added

---
+++ creating project part1/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    // for creating project with react 
+    // npm create vite@latest
+    // enter project name or if you want to create in current directory then type ./
+    //select react
+    // select js or ts
+    // cd projectname if you have created a other project name
+    // npm install - for installing the packages
+    // npm run dev - for running the project
+    <div>Welcome to react ki duniya.</div>
+  )
+}
+
+export default App
